cdc10d2bc7c0299647e7a41bd7526ac46b884b2a
Adam Beck <AdamB.Infinity@gmail.com>
2016-06-26 11:59:13 -0400

    Create simple example showing login
    * This is more or less a spike to proove the display on the page
    can be changed if a user is logged in or not

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import 'whatwg-fetch';
+import React, { Component } from 'react';
 
-const App = () => (
-  <div>
-    Placeholder for App
-  </div>
-);
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedIn: !!localStorage.getItem('token')
+    };
+
+    this.login = this.login.bind(this);
+    this.logOut = this.logOut.bind(this);
+
+  }
+
+  componentWillMount() {
+  }
+
+  storeToken(token) {
+    localStorage.setItem('token', token);
+    this.setState({
+      loggedIn: !!localStorage.getItem('token')
+    });
+  }
+
+  login() {
+    fetch('/api/auth', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: 'johnsmith4',
+        password: 'helloworld'
+      })
+    }).then(response => response.json())
+      .then(json => {
+        this.storeToken(json.token);
+      });
+  }
+
+  logOut() {
+    localStorage.removeItem('token');
+    this.setState({
+      loggedIn: false
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.loggedIn ? 'You are logged in!' : 'Sorry, you are not logged in'}
+
+        <div>
+          <button onClick={this.login}>Log In</button>
+          <button onClick={this.logOut}>Log Out</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
